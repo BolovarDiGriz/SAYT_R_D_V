f27ba8bd8797c60629bcdea580b9df1c14af37ba
BolovarDiGriz <80467971+BolovarDiGriz@users.noreply.github.com>
2021-06-21 14:36:58 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+border-radius: 7px;border: 5px solid #DCDCDC;width: 906px;height:310px;position:absolute;left:50px;right:100px;top:180px;margin: 5px auto;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center" >
